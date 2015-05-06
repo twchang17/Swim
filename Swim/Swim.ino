a5ef50a69ff73eefc5fe8fa305ec9265168eddab
twchang17 <twchang17@punahou.edu>
2015-05-05 14:19:19 -1000

    Workday
    Got a player and an enemy. Still working on movement. Enemy is moving
    along side with player but not in the right way.

--- Swim/Swim.ino
+++ Swim/Swim.ino
@@ -1,22 +1,116 @@
 
-#include <MeggyJrSimple.h>    // Required code, line 1 of 2.
-
-void setup()                    // run once, when the sketch starts
+#include <MeggyJrSimple.h>    
+int X=1;
+int Y=1;
+int X1;
+int X2;
+int X3;
+int Y1;
+int Y2;
+int Y3;
+void setup()                 
 {
-  MeggyJrSimpleSetup();      // Required code, line 2 of 2.
+  MeggyJrSimpleSetup();     
 }
 
-void loop()                     // run over and over again
+void loop()                   
 {
+  DrawPx(X,Y,1);
+  DrawPx(X1,X2,Green);
+  DisplaySlate();
+  ClearSlate(); 
+  playerMovement();
+  //updateEnemies
+  //updateAmmo
+  enemy1();
   
-  DrawPx(3,4,Yellow);           // Draw a dot at x=3, y=4, in yellow.
-  DisplaySlate();                  // Write the drawing to the screen.
-  delay(1000);                  // waits for a second
-  
-  ClearSlate();                 // Erase drawing
-  DisplaySlate();                  // Write the (now empty) drawing to the screen.
-   
-  delay(1000);                  // waits for a second
+                  
 }
 
+void playerMovement()
+{
+  CheckButtonsPress(); //up
+  if (Button_Up)
+  {
+    Y++;
+  }
+  
+  if (Button_Down) //down
+  {
+    Y--;
+  }  
+  
+  if(Button_Left) //left
+  {
+    X--; 
+  }
+  
+  if(Button_Right) //right
+  {
+    X++;
+  }     
+  
+  if(X>7)
+  {  
+    X--;
+  }
+  
+  if(X<0)
+  {
+    X++;
+  }
+  
+  if(Y>7)
+  {
+    Y--;
+  }
+  
+  if(Y<0)
+  {
+    Y++;
+  }
+}
 
+void enemy1()
+{
+  if(Button_Left) 
+  { 
+    if(X>X1)
+    X1++;
+    if(X<X1)
+    X1--;
+  }
+  if(Button_Right)
+  { 
+    if(X>X1)
+    X1++;
+    if(X<X1)
+    X1--;
+  }  
+  if(Button_Up) 
+  { 
+    if(Y>Y1)
+    Y++;
+    if(Y<Y1)
+    Y--;
+  }
+  if(Button_Down) 
+  { 
+    if(Y>Y1)
+    Y++;
+    if(Y<Y1)
+    Y--;
+  }
+}
+// enemy is moving along with player
+// can't fully move player, maybe enemy code is preventing
+void flag()
+{
+  DrawPx(7,2,Blue);
+}
+
+  
+  
+
+    
+    
