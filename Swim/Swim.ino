ae040ce37c7074ef80fbdfc62afc775b0c54474e
twchang17 <twchang17@punahou.edu>
2015-05-19 11:32:51 -1000

    Workday 5
    Have ideas, don’t know how to implement.

--- Swim/Swim.ino
+++ Swim/Swim.ino
@@ -1,8 +1,10 @@
-#include <MeggyJrSimple.h>    
-int X=1;
-int Y=1;
+#include <MeggyJrSimple.h> 
 
-struct Point{
+int X=1; //player x
+int Y=1; //player y
+
+struct Point
+{
   int x;
   int y;
 };
@@ -12,10 +14,10 @@ Point p2 = {1,7};
 Point p3 = {7,0};
 Point e[64] = {p1,p2,p3};
 
-boolean gameOver = false;
-boolean X1Alive = true;
-boolean X2Alive = true;
-boolean X3Alive = true;
+boolean gameOver = false; //running game while false, when true displays X
+boolean X1Alive = true; // enemy 1 alive
+boolean X2Alive = true; // enemy 2 alive
+boolean X3Alive = true; //enemy 3 alive
 
 void setup()                 
 {
@@ -28,15 +30,15 @@ void loop()
 {
   DrawPx(X,Y,White); //player
   if(X1Alive)
-    DrawPx(e[0].x,e[0].y,Green); //enemy 1 (drawings)
+    DrawPx(e[0].x,e[0].y,Green); //enemy 1 (drawings) Green
   else
     DrawPx(e[0].x,e[0].y,Red);
   if(X2Alive)
-    DrawPx(e[1].x,e[1].y,Blue); //enemy 2 (drawings)
+    DrawPx(e[1].x,e[1].y,Blue); //enemy 2 (drawings) Blue
   else
     DrawPx(e[1].x,e[1].y,Red);
   if(X3Alive)
-    DrawPx(e[2].x,e[2].y,Orange); //enemy 3 (drawings)
+    DrawPx(e[2].x,e[2].y,Orange); //enemy 3 (drawings) Orange
   else
     DrawPx(e[2].x,e[2].y,Red);
   DisplaySlate();
@@ -264,6 +266,23 @@ if(e[1].x==e[2].x && e[1].y==e[2].y)
     X3Alive = false;
   }  
 }
+
+void bomb() {
+  SetAuxLEDs(5);
+  if(Button_B){
+    DrawPx(X++1 , Y++1)
+    DisplaySlate();
+    delay(100);
+    ClearSlate();
+ }
+
+// lv2. e1 (6,6) ; e2(5,1) ; e3(0,7) ; e4(3,4)
+// lv3. e1 (6,0) ; e2(7,1) ; e3(3,5) ; e4(2,3) ; e5(0,4)
+// don't know if i should add more enemies, becomes near impossible
+// how to add levels?? 
+
+
+   
 
 
     
